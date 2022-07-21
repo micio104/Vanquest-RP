a9d6096620c18343cfbf743e7464168f7982ca93
Micio104 <gattodiego2007@gmail.com>
2022-07-21 19:13:27 +0200

    ..

---
+++ config.js
@@ -0,0 +1,111 @@
+var config = {
+    'title': '',
+    'welcomeMessage': 'I NOSTRI SPONSOR',
+    // Add images for the loading screen background.
+    images: [
+    'https://i.imgur.com/eB6LGQj.png', 'https://i.imgur.com/dpKBz2a.png',
+    ],
+    // Turn on/off music
+    enableMusic: true,
+    // Music list. (Youtube video IDs). Last one should not have a comma at the end. eduardo torres 58 9500 9800
+    music: [
+      'yscyWJziBMc'
+    ],
+    // Change music volume 0-100.
+    musicVolume: 50,
+    // Change Discord settings and link.    
+    'discord': {
+      'show': true,
+      'discordLink': 'discord.gg/UfhSUJxKT9',
+    },
+    // Change which key you have set in guidehud/client/client.lua
+    //'menuHotkey': 'F1',
+    // Turn on/off rule tabs. true/false
+    'rules': {
+      'generalconduct': true,
+      'roleplaying': true,
+      'rdmvdm': true,
+      'metagaming': true,
+      'newlife': true,
+      'abuse': true,
+    },
+  }
+  
+  // Home page annountments.
+  var announcements = [
+    '',
+  ]
+  
+  
+  // Add/Modify rules below.
+  var guidelines = [
+    'Violare il regolamento comporterà seri provvedimenti.',
+    'Se vedete qualcuno violare le regole, segnalatelo.',
+    'Violare regole comuni porterà al ban.',
+    'Tutte le regole sono disponibili in discord.',
+  ]
+  
+  var generalconduct = [
+    'Razzismo e Toxic non sono tollerati.',
+    'Vietato ruolare stupri o violenze sessuali, devono essere approvati dallo staff.',
+  ]
+  
+  var roleplaying = [
+    'I giocatori devono ruolare qualsiasi situazione.',
+    'I giocatori devono ruolare gli incidenti.',
+    'Vietato trollare.',
+    'Non si possono rubare veicoli quali polizia e ambulanza.',
+    'Vietato uscire fuori RP.',
+  ]
+  
+  var rdmvdm = [
+    'Il giocatore non può uccidere senza un motivo RP.',
+    'Vietato investire altri giocatori senza un apparente motivo RP.',
+  ]
+  
+  var metagaming = [
+    'Non è tollerato usare informazioni fuori dal gioco in RP.',
+  ]
+  
+  var newlife = [
+    'Il permadeath deve essere assistito da almeno 2 admin.',
+    'Una volta accettato il player dimenticherà tutto il suo trascorso.',
+  ]
+  
+  var abuse = [
+    'Vietato qualsiasi uso di cheat.',
+    'Segnala sempre chi usa hack.',
+    'Vietato abusare di bug.',
+  ]
+  
+  // Modify hotkeys below.
+  var generalhotkeys = [
+    'Clica <kbd>L</kbd> para abrir o telemovel.',
+    'Clica <kbd>K</kbd> para abrir o menu.',
+  ]
+  
+  var rphotkeys = [
+    'Escreve <code>/twt {mensagem}</code> no chat para usar o twitter.',
+    'Escreve <code>/ic {mensagem}</code> no chat para falar em rp.',
+    'Escreve <code>/forarp {mensagem}</code> no chat para falar fora de rp.',
+    'Escreve <code>/olx {mensagem}</code> no chat para venderes/comprares veiculos.',
+    'Escreve <code>/ilegal {mensagem}</code> no chat para falares em anonimo.',
+    'Pressiona <kbd>HOME</kbd> para veres a lista de jogadores.',
+    'Pressiona <kbd>X</kbd> para alevantar as mãos.',
+    'Pressiona <kbd>DELETE</kbd> para desmaiar.',
+    'Press <kbd>PAGEUP</kbd> para mudar o range de falar.',
+    'Pressiona <kbd>LEFT CTRL</kbd> para agachar.',
+    'Pressiona <kbd>B</kbd> para apontar.',
+  ]
+  
+  var vehiclehotkeys = [
+    'Pressiona <kbd>F3</kbd> para trancar/destrancar o seu veiculo.',
+    'Pressiona <kbd>G</kbd> para colocares o cinto.',
+    'Pressiona <kbd>Q</kbd> para ligar as luzes de emergencia',
+    'Pressiona <kbd>LEFT ALT</kbd> para ligar as sirenes de emergencia',
+    'Pressiona <kbd>R</kbd> para mudar o som da sirene de emergencia',
+  ]
+  
+  /* var jobshotkeys = [
+    'Press <kbd>F6</kbd> to bring up your job menu.',
+  ] */
